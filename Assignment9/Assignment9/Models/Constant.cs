e625c450f3ed7842946ea0b3d2e4135c689b0faf
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-02-17 13:15:52 +0200

    first commit

---
+++ Assignment9/Assignment9/Models/Constant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment9.Models
+{
+    public class Constant
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "cdd75d13d95ec5cd8c68932d887be3d9";
+    }
+}
